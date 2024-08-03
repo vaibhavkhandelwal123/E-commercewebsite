08cb6fcef8bd3f0f9ada079bc7f8d243373b78dc
vaibhav khandelwal <vaibhav@vaibhav.bbrouter>
2024-08-03 19:32:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import Navigation from './customer/components/Navigation/Navigation';
 import HomePage from './customer/pages/HomePage/HomePage';
 import Footer from './customer/components/Footer/Footer';
 import Product from './customer/components/Product/Product';
+import ProductDetails from './customer/components/ProductDetails/ProductDetails';
 
 function App() {
   return (
     <div className="">
       <Navigation/>
       <div>
-        <Product/>
+        {/* <Product/> */}
         {/* <HomePage/> */}
+        <ProductDetails/>
       </div>
         <Footer/>
     </div>
-
+    
   );
 }
 
