1ffcd26efee6c67fddb8d0f60fd23ab73ae95ce3
vaibhav khandelwal <vaibhav@vaibhav.bbrouter>
2024-08-07 15:24:58 +0530

    first commit

---
+++ src/customer/components/Navigation/navigationData.js
@@ -0,0 +1,122 @@
+export const navigation = {
+    categories: [
+      {
+        id: 'women',
+        name: 'Women',
+        featured: [
+          {
+            name: 'New Arrivals',
+            href: "#",
+            imageSrc: 'https://tailwindui.com/img/ecommerce-images/mega-menu-category-01.jpg',
+            imageAlt: 'Models sitting back to back, wearing Basic Tee in black and bone.',
+          },
+          {
+            name: 'Basic Tees',
+            href: '#',
+            imageSrc: 'https://tailwindui.com/img/ecommerce-images/mega-menu-category-02.jpg',
+            imageAlt: 'Close up of Basic Tee fall bundle with off-white, ochre, olive, and black tees.',
+          },
+        ],
+        sections: [
+          {
+            id: 'clothing',
+            name: 'Clothing',
+            items: [
+              { name: 'Tops', id:"top" ,href:`{women/clothing/tops}` },
+              { name: 'Dresses',id:"women_dress", href: '#' },
+              { name: 'Pants',id:"women_pants"},
+              { name: 'Denim',id:"denim" },
+              { name: 'Sweaters',id:"women_sweaters" },
+              { name: 'T-Shirts',id:"'women_tshirts" },
+              { name: 'Jackets',id:"women_jackets" },
+              { name: 'Activewear',id:"Activewear"  },
+              { name: 'Browse All',id:"saree" },
+            ],
+          },
+          {
+            id: 'accessories',
+            name: 'Accessories',
+            items: [
+              { name: 'Watches', id: '#' },
+              { name: 'Wallets', id: '#' },
+              { name: 'Bags', id: '#' },
+              { name: 'Sunglasses', id: '#' },
+              { name: 'Hats', id: '#' },
+              { name: 'Belts', id: '#' },
+            ],
+          },
+          {
+            id: 'brands',
+            name: 'Brands',
+            items: [
+              { name: 'Full Nelson', id: '#' },
+              { name: 'My Way', id: '#' },
+              { name: 'Re-Arranged', id: '#' },
+              { name: 'Counterfeit', id: '#' },
+              { name: 'Significant Other', id: '#' },
+            ],
+          },
+        ],
+      },
+      {
+        id: 'men',
+        name: 'Men',
+        featured: [
+          {
+            name: 'New Arrivals',
+            href: '#',
+            imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-04-detail-product-shot-01.jpg',
+            imageAlt: 'Drawstring top with elastic loop closure and textured interior padding.',
+          },
+          {
+            name: 'Artwork Tees',
+            href: '#',
+            imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-06.jpg',
+            imageAlt:
+              'Three shirts in gray, white, and blue arranged on table with same line drawing of hands and shapes overlapping on front of shirt.',
+          },
+        ],
+        sections: [
+          {
+            id: 'clothing',
+            name: 'Clothing',
+            items: [
+              { name: 'T-shirts', id:"T-shirts", href:`{Men/clothing/t_shirts}` },
+              { name: 'Pants', id: '#' },
+              { name: 'Sweaters', id: '#' },
+              { name: 'T-Shirts', id: '#' },
+              { name: 'Jackets', id: '#' },
+              { name: 'Activewear', id: '#' },
+              { name: 'Browse All', id: '#' },
+            ],
+          },
+          {
+            id: 'accessories',
+            name: 'Accessories',
+            items: [
+              { name: 'Watches', id: '#' },
+              { name: 'Wallets', id: '#' },
+              { name: 'Bags', id: '#' },
+              { name: 'Sunglasses', id: '#' },
+              { name: 'Hats', id: '#' },
+              { name: 'Belts', id: '#' },
+            ],
+          },
+          {
+            id: 'brands',
+            name: 'Brands',
+            items: [
+              { name: 'Re-Arranged', id: '#' },
+              { name: 'Counterfeit', id: '#' },
+              { name: 'Full Nelson', id: '#' },
+              { name: 'My Way', id: '#' },
+            ],
+          },
+        ],
+      },
+    ],
+    pages: [
+      { name: 'Company', id: '/'},
+      { name: 'Stores', id: '/' },
+    ],
+  }
